374e6fc40eedc75f36d0f35cf8ab9f9461a37481
iamtun <letuan19431791.iuh@gmail.com>
2024-07-31 17:58:48 +0700

    feat: handle use session

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import Express from "express";
 import router from "./routers/index.js";
+import session from "express-session";
 
 const app = Express();
 const PORT = 3000;
 
 app.use(Express.json());
+app.use(
+  session({
+    secret: "book_review", // Replace with your secret key
+    resave: false, // Forces the session to be saved back to the session store
+    saveUninitialized: true, // Forces a session that is "uninitialized" to be saved to the store
+    cookie: { secure: false }, // Set to true if using HTTPS
+  })
+);
 
 app.use("/api", router);
 
